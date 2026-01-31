b85d1560ec0b9c181ab0e0befa2b125912b6be23
daivya4 <daivyarocks@gmail.com>
2026-01-31 18:24:41 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
-
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((error) => {
+    console.log("Error connecting to the database:",error)
+})
 
 
 
